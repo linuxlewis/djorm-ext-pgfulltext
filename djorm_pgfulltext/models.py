c60737fcccd919e1d8ab828b68a93be539fc1896
Marek Brzóska <brzoskamarek@gmail.com>
2012-12-06 11:21:52 +0100

    fix import (broken for django 1.3 and 1.4)

--- djorm_pgfulltext/models.py
+++ djorm_pgfulltext/models.py
@@ -4,7 +4,7 @@ from itertools import repeat
 
 # Python3 compatibility
 try:
-    from django.utils.encoding import force_text as force_text
+    from django.utils.encoding import force_unicode as force_text
 except ImportError:
     from django.utils.encoding import force_text
 
