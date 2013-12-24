8a797bf1bf7285a6f58a7a66e2302def1c48522e
Andrey Antukh <niwi@niwi.be>
2013-12-24 17:11:02 +0100

    Increment version to 0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ PostgreSQL Full Text Search integration with django orm.
 
 setup(
     name = "djorm-ext-pgfulltext",
-    version = '0.8',
+    version = '0.9',
     url = 'https://github.com/niwibe/djorm-ext-pgfulltext',
     license = 'BSD',
     platforms = ['OS Independent'],
